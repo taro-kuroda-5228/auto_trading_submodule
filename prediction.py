bfcbcabf2a47f486427476a36dc2a94694850c30
taro-kuroda-5228 <dancing205228@gmail.com>
2022-03-04 20:46:13 +0900

    Update:prediction.py

--- prediction.py
+++ prediction.py
@@ -1,5 +1,8 @@
 from model_selection import ModelSelection
 from sklearn.metrics import accuracy_score
+import lightgbm as lgb
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 class Prediction:
@@ -23,12 +26,38 @@ class Prediction:
         self.l_X_test = l_X_test
         self.l_y_train = l_y_train
         self.l_y_test = l_y_test
+        
+    def fit(self):
+        clf = lgb.LGBMClassifier()
+        self.best_model, self.idx_max_split = clf, 4
+        self.best_model.fit(self.l_X_train[self.idx_max_split], self.l_y_train[self.idx_max_split])
+        
+    def plot(self):
+        self.fit()
+        self.booster = self.best_model.booster_
+        lgb.plot_importance(self.booster, importance_type="gain")
+#         plt.show()
+        
+    def importance(self):
+        self.plot()
+        self.feat_imp = pd.DataFrame(
+            {
+                "特徴量": self.booster.feature_name(),
+                "変数重要度": self.booster.feature_importance(importance_type="gain"),
+            }
+        )
+        self.feat_imp["変数重要度"] = self.feat_imp["変数重要度"] / self.feat_imp["変数重要度"].sum()
+        self.tmp = (
+            self.feat_imp.sort_values(by="変数重要度", ascending=False)
+            .reset_index(drop=True)
+            .reset_index()
+        )
+        self.tmp["index"] = self.tmp["index"] + 1
+        self.tmp.rename(columns={"index": "損失関数の減少に貢献したランキング"})
+        return self.tmp.head()
 
     def pred(self):
-        self.best_model, self.idx_max_split = self.model_selection.best_model()
-        self.best_model.fit(
-            self.l_X_train[self.idx_max_split], self.l_y_train[self.idx_max_split]
-        )
+        self.fit()
         self.pred = self.best_model.predict(self.l_X_test[self.idx_max_split])
         return self.pred
     
